b6443d2ef8e8085a9ea52cdcdd2c9d441bcfbe8f
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-06 16:35:11 +0200

    fetch star wars datas

--- App.tsx
+++ App.tsx
@@ -4,12 +4,17 @@ import LoginScreen from './src/screens/LoginScreen';
 import { Card } from 'react-native-paper';
 import FeedScreen from './src/screens/FeedScreen';
 import TermsScreen from './src/screens/TermsScreen';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useStarships } from './hooks/useStarships';
+
+const queryClient = new QueryClient()
 
 const App = () => {
   return (
     // <LoginScreen />
     // <TermsScreen />
     <FeedScreen />
+    // <QueryClientProvider client={queryClient}>...</QueryClientProvider>
    );
 };
 
