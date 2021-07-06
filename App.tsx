623280903d265cfb1f4ad6303789d76e2b9896a7
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-06 12:03:45 +0200

    creating TermsScreen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-// import LoginScreen from './src/screens/LoginScreen';
+import LoginScreen from './src/screens/LoginScreen';
 import { Card } from 'react-native-paper';
+import FeedScreen from './src/screens/FeedScreen';
+import TermsScreen from './src/screens/TermsScreen';
 
 const App = () => {
   return (
-  <View>
-    <Text>Hello</Text>
-  </View>
+    // <LoginScreen />
+    <TermsScreen />
+    // <FeedScreen />
    );
 };
 
