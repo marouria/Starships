5d49b0b8a56677c9c2bcdb28ced57a6fb5429af0
Marie Leuliette <marie.leuliette@gmail.com>
2021-07-06 11:12:42 +0200

    install xcode

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import LoginScreen from './src/screens/LoginScreen';
+import { View, Text, StyleSheet } from 'react-native';
+// import LoginScreen from './src/screens/LoginScreen';
+import { Card } from 'react-native-paper';
 
 const App = () => {
-  return <LoginScreen />;
+  return (
+  <View>
+    <Text>Hello</Text>
+  </View>
+   );
 };
 
 export default App;
